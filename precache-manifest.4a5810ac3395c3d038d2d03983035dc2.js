47e33b4916392305d08228c4caf30f44275b7a1d
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-07-08 16:40:16 +0530

    Updates

---
+++ precache-manifest.4a5810ac3395c3d038d2d03983035dc2.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a2167a4deccacd2bb6872fb300f15e",
+    "url": "/www.sabhishek.com/index.html"
+  },
+  {
+    "revision": "b5d7160e2a8dfceed273",
+    "url": "/www.sabhishek.com/static/css/main.e46b6af8.chunk.css"
+  },
+  {
+    "revision": "4c027e8840bd274b4fe9",
+    "url": "/www.sabhishek.com/static/js/2.ec953ecb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www.sabhishek.com/static/js/2.ec953ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d7160e2a8dfceed273",
+    "url": "/www.sabhishek.com/static/js/main.01c3247e.chunk.js"
+  },
+  {
+    "revision": "77173f64b6d91de00a5c",
+    "url": "/www.sabhishek.com/static/js/runtime-main.cbf1e4a8.js"
+  },
+  {
+    "revision": "394860ad574872bb118b43594e163287",
+    "url": "/www.sabhishek.com/static/media/agg1.394860ad.png"
+  },
+  {
+    "revision": "45e04bffc01132051cdc945487d17a9d",
+    "url": "/www.sabhishek.com/static/media/agg2.45e04bff.png"
+  },
+  {
+    "revision": "dd8606e5390351abaa207c95599c80c4",
+    "url": "/www.sabhishek.com/static/media/agg3.dd8606e5.png"
+  },
+  {
+    "revision": "5e1f611e35da982de3797b62f14c87b2",
+    "url": "/www.sabhishek.com/static/media/aws_logo.5e1f611e.png"
+  },
+  {
+    "revision": "fc664ed6812d4a4158e34e5040ad3654",
+    "url": "/www.sabhishek.com/static/media/background.fc664ed6.jpg"
+  },
+  {
+    "revision": "c3238a20d56840eb5e9fa80f9de27332",
+    "url": "/www.sabhishek.com/static/media/carousel1.c3238a20.png"
+  },
+  {
+    "revision": "3db2c4ba961e62fd6ed4da206f78df9d",
+    "url": "/www.sabhishek.com/static/media/carousel2.3db2c4ba.png"
+  },
+  {
+    "revision": "f9cdda47db4543d4339a7e9d80251798",
+    "url": "/www.sabhishek.com/static/media/carousel3.f9cdda47.png"
+  },
+  {
+    "revision": "4b24347f080299317d431c8c99ede6f0",
+    "url": "/www.sabhishek.com/static/media/ce1.4b24347f.png"
+  },
+  {
+    "revision": "6275892d25a519697b30f69ab16b08c2",
+    "url": "/www.sabhishek.com/static/media/ce2.6275892d.png"
+  },
+  {
+    "revision": "489763369913e172376a36c1a493a9a7",
+    "url": "/www.sabhishek.com/static/media/ce3.48976336.png"
+  },
+  {
+    "revision": "a2a796f42a7240811dcddc434f1a9827",
+    "url": "/www.sabhishek.com/static/media/climate1.a2a796f4.png"
+  },
+  {
+    "revision": "ab18f56a15c55fb4d4d88fb2f0edf3a1",
+    "url": "/www.sabhishek.com/static/media/climate2.ab18f56a.png"
+  },
+  {
+    "revision": "93b56294fab1b8d4113d186268e42071",
+    "url": "/www.sabhishek.com/static/media/cloudintegration.93b56294.png"
+  },
+  {
+    "revision": "fb80f3760f0650962203962a98e20e0c",
+    "url": "/www.sabhishek.com/static/media/crm1.fb80f376.jpg"
+  },
+  {
+    "revision": "0ccd75e19d938639d130b2916c97773e",
+    "url": "/www.sabhishek.com/static/media/crm2.0ccd75e1.jpg"
+  },
+  {
+    "revision": "1eb699a54c8e0f48bc02be07aadb8c8f",
+    "url": "/www.sabhishek.com/static/media/crm3.1eb699a5.jpg"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/www.sabhishek.com/static/media/cssLogo.c26a73b4.png"
+  },
+  {
+    "revision": "1c95909fa88358ae7605ff61377364e4",
+    "url": "/www.sabhishek.com/static/media/databasedevelopment.1c95909f.jpg"
+  },
+  {
+    "revision": "fb17a36496b27e71c1a05888cdb4b632",
+    "url": "/www.sabhishek.com/static/media/facebookIcon.fb17a364.webp"
+  },
+  {
+    "revision": "e642c7556d83be574ae3929c34ead074",
+    "url": "/www.sabhishek.com/static/media/firebaseLogo.e642c755.png"
+  },
+  {
+    "revision": "17a6652f345bc5ea2db2d4fba0c95a84",
+    "url": "/www.sabhishek.com/static/media/githubIcon.17a6652f.png"
+  },
+  {
+    "revision": "495d6dbbcbc96f8a27842f0ef7c43d3a",
+    "url": "/www.sabhishek.com/static/media/githubprofile.495d6dbb.jpg"
+  },
+  {
+    "revision": "6be84212af97f490f1ad8f060347472c",
+    "url": "/www.sabhishek.com/static/media/hackerTimesDesktop1.6be84212.jpg"
+  },
+  {
+    "revision": "2173b2750c1aac7f0f4266154b3d7c25",
+    "url": "/www.sabhishek.com/static/media/hackerTimesDesktop2.2173b275.jpg"
+  },
+  {
+    "revision": "ed468df34da5bd89faf07e404629fe6a",
+    "url": "/www.sabhishek.com/static/media/hackerTimesDesktop3.ed468df3.jpg"
+  },
+  {
+    "revision": "1b05b34cfff4b357489f30c72280f9fa",
+    "url": "/www.sabhishek.com/static/media/jsLogo.1b05b34c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/www.sabhishek.com/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34cbb296915e80181a5fe11c5072ad61",
+    "url": "/www.sabhishek.com/static/media/maps1.34cbb296.png"
+  },
+  {
+    "revision": "c0d3ab372684c17a6035cee4cb268e90",
+    "url": "/www.sabhishek.com/static/media/maps2.c0d3ab37.png"
+  },
+  {
+    "revision": "e834e79a201b7c405daffe61fdcfc139",
+    "url": "/www.sabhishek.com/static/media/maps3.e834e79a.png"
+  },
+  {
+    "revision": "d9c541880c4cc739127bf47509f208ad",
+    "url": "/www.sabhishek.com/static/media/markdown.d9c54188.jpg"
+  },
+  {
+    "revision": "d3643da6aec641542513bedc38e7f654",
+    "url": "/www.sabhishek.com/static/media/mcTimes1.d3643da6.png"
+  },
+  {
+    "revision": "d653fc7bb125b2643efdfdbeb572154e",
+    "url": "/www.sabhishek.com/static/media/mcTimes2.d653fc7b.png"
+  },
+  {
+    "revision": "667b1a6ee566891cb043b468acbf74fa",
+    "url": "/www.sabhishek.com/static/media/mcTimes3.667b1a6e.png"
+  },
+  {
+    "revision": "b977184f38aa778df1bee2c6d96fd03a",
+    "url": "/www.sabhishek.com/static/media/mcTimes4.b977184f.png"
+  },
+  {
+    "revision": "167d402f4a928898904a8761a24240bb",
+    "url": "/www.sabhishek.com/static/media/mobileappdevelopment.167d402f.png"
+  },
+  {
+    "revision": "2ef2f4dac8dd3a29ffedc6c40df2462f",
+    "url": "/www.sabhishek.com/static/media/mongodb.2ef2f4da.webp"
+  },
+  {
+    "revision": "fb381ca75629cbbb0e4a98bc511b81a7",
+    "url": "/www.sabhishek.com/static/media/movies1.fb381ca7.jpg"
+  },
+  {
+    "revision": "00aadd9dd2c49dd5dffac6ccb98e6844",
+    "url": "/www.sabhishek.com/static/media/nyt1.00aadd9d.png"
+  },
+  {
+    "revision": "ef4efed917d87abd94127f357371d0f3",
+    "url": "/www.sabhishek.com/static/media/nyt2.ef4efed9.png"
+  },
+  {
+    "revision": "2c164836be53251465a67befb2ad6187",
+    "url": "/www.sabhishek.com/static/media/nyt3.2c164836.png"
+  },
+  {
+    "revision": "21f6c823cfec15da6e61db963a8381b7",
+    "url": "/www.sabhishek.com/static/media/nyt4.21f6c823.png"
+  },
+  {
+    "revision": "b4b838483b07168a8bae472604825db4",
+    "url": "/www.sabhishek.com/static/media/profilepic.b4b83848.jpg"
+  },
+  {
+    "revision": "bbce086ff51edbd4d1302656644196ee",
+    "url": "/www.sabhishek.com/static/media/quiz1.bbce086f.jpg"
+  },
+  {
+    "revision": "122f2337313bc52a01925118adad2d99",
+    "url": "/www.sabhishek.com/static/media/quiz3.122f2337.jpg"
+  },
+  {
+    "revision": "6a2ada844e2f75d1a5b8ecc43dffacc8",
+    "url": "/www.sabhishek.com/static/media/quiz4.6a2ada84.jpg"
+  },
+  {
+    "revision": "e0f16f72190508f719aa6ec1e188bc6b",
+    "url": "/www.sabhishek.com/static/media/quotes1.e0f16f72.png"
+  },
+  {
+    "revision": "2656f951c1a11d78ba9e96128e7651a4",
+    "url": "/www.sabhishek.com/static/media/quotes2.2656f951.png"
+  },
+  {
+    "revision": "a41684561ce7c10c1254e7bd8affa058",
+    "url": "/www.sabhishek.com/static/media/quotes3.a4168456.png"
+  },
+  {
+    "revision": "a0e31fb74a78632ad2e68fbb5fd8d597",
+    "url": "/www.sabhishek.com/static/media/quotes4.a0e31fb7.png"
+  },
+  {
+    "revision": "aa94f0872d0ecd2a058177f1a8015415",
+    "url": "/www.sabhishek.com/static/media/spotify1.aa94f087.png"
+  },
+  {
+    "revision": "4d11b93703e1f675162769786d06df3c",
+    "url": "/www.sabhishek.com/static/media/spotify2.4d11b937.png"
+  },
+  {
+    "revision": "231920afc854ae3bf3d49460854e5495",
+    "url": "/www.sabhishek.com/static/media/spotify3.231920af.png"
+  },
+  {
+    "revision": "1aca76691b21c39f88a3999747bec0a3",
+    "url": "/www.sabhishek.com/static/media/spotify4.1aca7669.png"
+  },
+  {
+    "revision": "49186d24a84c4620d123e80de3044194",
+    "url": "/www.sabhishek.com/static/media/tictactoe1.49186d24.png"
+  },
+  {
+    "revision": "d2f9150ea0eb357956f64c6d955d5ab7",
+    "url": "/www.sabhishek.com/static/media/tictactoe2.d2f9150e.png"
+  },
+  {
+    "revision": "1927b31d8a9b19c7451c679082924c52",
+    "url": "/www.sabhishek.com/static/media/tictactoe3.1927b31d.png"
+  },
+  {
+    "revision": "18745b3b4ca612c322c34109fcaad23f",
+    "url": "/www.sabhishek.com/static/media/timer.18745b3b.jpg"
+  },
+  {
+    "revision": "50329c1660521cf6fdf5dfe2f32ed595",
+    "url": "/www.sabhishek.com/static/media/webdevelopment.50329c16.jpg"
+  },
+  {
+    "revision": "29cfd180e3f85144fac58035f29b0f15",
+    "url": "/www.sabhishek.com/static/media/whatsapp.29cfd180.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio_abhishek/precache-manifest.4d3a2afcd8c1418b97cbb1dea37c8a4c.js"
+  "/www.sabhishek.com/precache-manifest.4a5810ac3395c3d038d2d03983035dc2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_abhishek/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.sabhishek.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
